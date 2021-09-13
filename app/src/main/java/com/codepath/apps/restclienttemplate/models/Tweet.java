f98aea05e95a0dc8d597e8caa1b8b8c4bb19e5d8
Lorenzo Miro San Diego <lsandiego2@horizon.csueastbay.edu>
2021-09-12 21:44:29 -0700

    User + Tweet classes implemented

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,27 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONObject;
+
+public class Tweet {
+
+    public String body;
+
+    public String created_at;
+
+    public User user;
+
+    public static Tweet fromJson(JSONObject jsonObject){
+
+        Tweet tweet = new Tweet();
+
+        tweet.body = jsonObject.getString("text");
+
+        tweet.created_at = jsonObject.getString("created_at");
+
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+
+        return tweet;
+
+    }
+
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,46 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    public String name;
+
+    public String screen_name;
+
+    public String public_image_url;
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+
+        User user = new User();
+
+        user.name = jsonObject.getString("name");
+
+        user.screen_name = jsonObject.getString("screen_name");
+
+        user.public_image_url = jsonObject.getString("public_image_url_https");
+
+        return user;
+
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+
+        List<Tweet> tweets = new ArrayList<>();
+
+        for (int p = 0; p < jsonArray.length(); p++) {
+
+            tweets.add(Tweet.fromJson(jsonArray.getJSONObject(p)));
+
+        }
+
+        return tweets;
+
+    }
+
+}
